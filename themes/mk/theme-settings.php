255997e93572b8e9fafc0e8f8898a6e18e904b14
Alex Net <alexnet2004@gmail.com>
2017-12-26 23:40:03 +0300

    Ввернули в настройки темы . текст и сумму наценки Теперь можно править из админки

--- themes/mk/theme-settings.php
+++ themes/mk/theme-settings.php
@@ -21,6 +21,24 @@ function mk_form_system_theme_settings_alter(&$form){
 			'#default_value'=>empty($fd['footer-right'])?'':$fd['footer-right'],
 		),
 	);
+	$fd=theme_get_setting('nacenka');
+	$form['nacenka']=array(
+		'#type'=>'fieldset',
+		'#title'=>'Данные наценки',
+		'#tree'=>true,
+		'text'=>array(
+			'#type'=>'textfield',
+			'#title'=>'Текст наценки',
+			'#default_value'=>empty($fd['text'])?'':$fd['text'],
+		),
+		'size'=>array(
+			'#type'=>'textfield',
+			'#title'=>'Наценочный порог',
+			'#description'=>'Сумма заказа после перехода через которую наценка не будет действовать',
+			'#default_value'=>empty($fd['size'])?0:$fd['size'],
+			'#field_suffix'=>'р.',
+		),
+	);
 	//dsm($form);
 }
 ?>
