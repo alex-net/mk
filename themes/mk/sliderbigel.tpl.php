949e0ddeace18a4d9531cf5dcc3152b589f4162d
Alex Net <alexnet2004@gmail.com>
2018-01-30 22:05:53 +0300

    устранили багу со слайдером акций ..
    выводилось одно и тоже описание для каждого товара. причина: в шаблоне не был заменён html

--- themes/mk/sliderbigel.tpl.php
+++ themes/mk/sliderbigel.tpl.php
@@ -1,7 +1,7 @@
 <?php foreach($data as $v):?>
 <div class="akcion-big-el clearfix">
 	<div class="col-sm-21 col-md-21"><h2><span><?php echo $v['titletop'];?></span><span><?php echo $v['titlebottom'];?></span></h2>
-	<p>Битумный праймер предназначен для обработки бетонных и цементно-песчаных стяжек перед началом кровельных или гидроизоляционных работ. Позволяет...</p>
+	<div class="body"><?php echo $v['body'];?></div>
 	<div class="price_buy_button">
 	<div class="price_discount_wrap"><span class="discount_price">Акционная цена: <?php echo $v['cp'];?>  / <span><?php echo $v['measure'];?></span></span></div>
 	<a href="<?php echo $v['url'];?>" class="buy_prod_button">подробнее</a>
