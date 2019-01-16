69a91dcb1001b8753999bd411d69aa3ea75578e1
Alex Net <alexnet2004@gmail.com>
2019-01-16 20:57:23 +0300

    правки по оформлению. карточек товара..

--- modules/addons/mkmod/mkmod-formaddtocart.js
+++ modules/addons/mkmod/mkmod-formaddtocart.js
@@ -21,7 +21,7 @@
 								t=Math.round(parseFloat(data[i]['commerce_price'])*val*100)/100;
 								if (t-Math.round(t)==0)
 									t+='.0';
-								put.html('Итого: '+(t)+' руб');
+								put.html('Итого: <span>'+(t)+' руб</span>');
 						}
 							//console.log(data[i]);
 					if (isNaN(val))
