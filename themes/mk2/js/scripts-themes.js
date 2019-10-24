da39b800b62481fe124b1a7330ed46f450726462
Alex Net <alexnet2004@gmail.com>
2019-10-25 00:48:33 +0300

    Накрутили на каталог на главной третий уровень ..
    Появлятся на ховере .. второго уровня ...)

--- themes/mk2/js/scripts-themes.js
+++ themes/mk2/js/scripts-themes.js
@@ -11,6 +11,24 @@
 			el.attr('href',items[k].href);
 			el.addClass('s-item');
 			el.append('<span class="name">'+items[k].name+'</span>');
+			if (items[k].childs){
+				el.find('span.name').wrap('<span class="menu-wrap"/>');
+				for(var i in items[k]['childs-list']){
+					var subel=$('<span class="subel" />');
+					subel.data('link',items[k]['childs-list'][i].url);
+					subel.append(items[k]['childs-list'][i].name);
+					subel.on('click',function(e){
+						e.preventDefault();
+						location.href=$(this).data('link');
+						//console.log();
+						
+						//return false;
+
+					});
+					el.find('span.menu-wrap').append(subel);
+				}
+			}
+
 			el.css({'background-image':'url('+items[k].img+')'});
 			el.data('id',items[k].id);
 			container=container.add(el);
