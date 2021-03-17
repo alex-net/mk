9a144035c32d675a3cb6877c14d3ed0cffd74192
Alex Net <alexnet2004@gmail.com>
2021-03-18 01:03:27 +0300

    Обновили оформление для всплывашки информера после добавления товара в корзину

--- themes/mk2/node--product-display--inpopup.tpl.php
+++ themes/mk2/node--product-display--inpopup.tpl.php
@@ -6,12 +6,13 @@
 			<?=render($content);?>
 			<?php if(!empty($content['cart-data'])):?>
 				<div class='form-text-place'>
-					<div class='one-product'><?=$content['cart-data']['#added-now'];?> <?=$content['cart-data']['#unit']; ?> = <?=$content['cart-data']['#price-from-product']; ?> руб.</div>
+					<div class='price-of-product'>Цена за <?=$content['cart-data']['#unit']; ?> = <?=$content['cart-data']['#price-from-product']; ?> руб.</div>
+					<div class="qty-added">Количество <?=$content['cart-data']['#added-now'];?> <?=$content['cart-data']['#unit']; ?></div>
+
 					<div class='total-current-added'>Всего = <?=$content['cart-data']['#summ-now']; ?> руб.</div>
 				</div>
-				
+
 			<?php endif;?>
 		</div>
 	</div>
-	
 </div>
